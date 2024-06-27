89505855704031267510e2f569fbec376a91ac94
kumaresan9176773252 <kumara2275012@gmail.com>
2024-06-27 14:33:48 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:3JmvUIsYFl2QCIKM@cluster0.l2h9kjy.mongodb.net/bookStore?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
+
+  //3JmvUIsYFl2QCIKM

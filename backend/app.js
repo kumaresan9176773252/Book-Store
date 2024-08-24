577736e834c90a582404e78bb7e37a983eab51f6
kumaresan9176773252 <kumara2275012@gmail.com>
2024-08-24 10:17:34 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://book-store-frontend-hx58.onrender.com', // Replace with your actual frontend URL
+  methods: 'GET,POST,PUT,DELETE',
+}));
 app.use("/books", router); // localhost:5000/books
 
 mongoose
